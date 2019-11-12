862564b5c9b188f1c8ef818a87dc913e37f1c11c
Ritvik Iyer <rikiiyer@gmail.com>
2019-11-11 16:01:55 -0800

    consolidate

--- stratified_sampling.py
+++ stratified_sampling.py
@@ -1,16 +1,15 @@
 import pandas as pd
 
-#read in the original data
-original_data =  pd.read_csv('113Adata.csv')
+def takeTableSample(data, column, feature, year):
+    data = data[data['year'] == year]
+    percentage = data[column].value_counts(normalize=True, sort=True)[feature]
+    sample_size = 160000 * percentage
+    data = data[data[column] == feature]
+    return data.sample(int(sample_size))
 
-#almost all missing values for years in 2017, so drop that column
-original_data = original_data.drop(columns = ['wkswork1'])
-
-#filter out missing values (NaNs) to produce clean dataframe
-cleaned = original_data.dropna()
-
-#reduce file size by performing stratified sampling on year, randomly select 160K rows from each year
-sampled = cleaned.groupby('year', group_keys=False).apply(lambda x: x.sample(160000))
-
-#export the dataframe as a csv file
-export_csv = sampled.to_csv('cp113.csv', index = None, header=True)
+def sampledTable(data, column, year):
+    sample = pd.DataFrame()
+    for feature in data[data['year'] == year][column].value_counts(normalize=True, sort=True).index:
+        new_sample = takeTableSample(data, column, feature, year)
+        sample = pd.concat([sample, new_sample])
+    return sample
